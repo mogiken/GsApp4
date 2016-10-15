a4009f8dde7cf1fd2c4a404077156af28ce2ca43
MSI\mogi <mogi@gashfara.com>
2016-10-15 11:54:53 +0900

    いいね完成

--- GsApp4/app/src/main/java/com/gashfara/mogi/gsapp4/MessageRecord.java
+++ GsApp4/app/src/main/java/com/gashfara/mogi/gsapp4/MessageRecord.java
@@ -6,12 +6,15 @@ public class MessageRecord {
     private String imageUrl;
     private String comment;
     private String id;
+    private int goodCount;
 
     //データを１つ作成する関数です。項目が増えたら増やしましょう。
-    public MessageRecord(String id,String imageUrl, String comment) {
+    public MessageRecord(String id,String imageUrl, String comment,int goodCount) {
         this.imageUrl = imageUrl;
         this.comment = comment;
         this.id = id;
+        this.goodCount = goodCount;
+
     }
     //それぞれの項目を返す関数です。項目が増えたら増やしましょう。
     public String getComment() {
@@ -23,5 +26,8 @@ public class MessageRecord {
     public String getId() {
         return id;
     }
+    public int getGoodCount() {return goodCount;}
+    //セットする関数.項目が増えたら追加しましょう
+    public void setGoodCount(int goodCount){this.goodCount = goodCount;}
 
 }
