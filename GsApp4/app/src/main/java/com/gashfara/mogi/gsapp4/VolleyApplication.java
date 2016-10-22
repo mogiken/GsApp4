9aa16bf098912cbf0b6664eb015cd5b105f81b67
MSI\mogi <mogi@gashfara.com>
2016-10-22 11:05:26 +0900

    完成

--- GsApp4/app/src/main/java/com/gashfara/mogi/gsapp4/VolleyApplication.java
+++ GsApp4/app/src/main/java/com/gashfara/mogi/gsapp4/VolleyApplication.java
@@ -6,7 +6,11 @@ package com.gashfara.mogi.gsapp4;
 import android.app.Application;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
 import com.kii.cloud.storage.Kii;
+
+import java.util.HashMap;
 
 //Applicationクラスを継承extend（コピーみたいなもの）しています。Applicationの機能がそのまま使えます。{}までがクラスです。
 //これはクラスの定義です。このクラスを使うにはnewなどをしてインスタンス化（実態を作る）してから使います。
@@ -15,8 +19,31 @@ import com.kii.cloud.storage.Kii;
 public class VolleyApplication extends Application {
     //クラスはこのようにintのような型として使えます。
     private static VolleyApplication sInstance;
-
     private RequestQueue mRequestQueue;
+    //GrowthHackで追加ここから
+    //トラッキングIDを設定
+    private static final String PROPERTY_ID = "UA-85738832-1";
+    public enum TrackerName {
+        APP_TRACKER, // Tracker used only in this app.
+        GLOBAL_TRACKER, // Tracker used by all the apps from a company. eg: roll-up tracking.
+    }
+    HashMap<TrackerName, Tracker> mTrackers = new HashMap<TrackerName, Tracker>();
+    //参考サイトのまま：http://qiita.com/chonbo2525/items/bbc55d728f8e1b8dca39
+    public synchronized Tracker getTracker(TrackerName trackerId) {
+        if (!mTrackers.containsKey(trackerId)) {
+
+            GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
+            Tracker t = (trackerId == TrackerName.APP_TRACKER) ? analytics.newTracker(PROPERTY_ID)
+                    : (trackerId == TrackerName.GLOBAL_TRACKER) ? analytics.newTracker(R.xml.global_tracker)
+                    : analytics.newTracker(R.xml.ecommerce_tracker);
+            t.enableAdvertisingIdCollection(true);
+            mTrackers.put(trackerId, t);
+
+        }
+        return mTrackers.get(trackerId);
+    }
+    //GrowthHackで追加ここまで
+
     //overrideは継承元のクラスApplicationの機能を引き継ぐのではなく上書きすることを宣言しています。
     //onCreateはアプリを起動した時にOSから呼び出される関数です。よく使います。
     @Override
@@ -30,7 +57,7 @@ public class VolleyApplication extends Application {
         sInstance = this;
         //Userで追加ここから
         //KiiCloudの初期化。Applicationクラスで実行してください。キーは自分の値にかえる。
-        Kii.initialize(getApplicationContext(), "321a5a4b", "a1cb0119557df5d260796d4053e05c05", Kii.Site.JP);
+        Kii.initialize(getApplicationContext(), "321a5a4b", "a1cb0119557df5d260796d4053e05c05", Kii.Site.JP,true);
         //Userで追加ここまで
 
     }
